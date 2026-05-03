413e586de99e914a6cb73544bf9ae021e072f497
PSH <psihyeon0921@gmail.com>
2026-05-04 02:23:43 +0900

    [5주차] 100까지 완료

---
+++ CryptRaider/Source/CryptRaider/TriggerComponent.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TriggerComponent.h"
+
+UTriggerComponent::UTriggerComponent()
+{
+    PrimaryComponentTick.bCanEverTick = true; // 이 부분이 true여야 합니다.
+}
+
+
+void UTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+   
+}
