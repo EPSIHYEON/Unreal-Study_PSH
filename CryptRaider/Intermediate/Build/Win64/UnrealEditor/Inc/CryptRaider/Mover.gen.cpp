bea810cf095968ac91f35b1a51358222cf597dc9
PSH <psihyeon0921@gmail.com>
2026-05-11 03:36:16 +0900

    [5주차] 106까지 완료

--- CryptRaider/Intermediate/Build/Win64/UnrealEditor/Inc/CryptRaider/Mover.gen.cpp
+++ CryptRaider/Intermediate/Build/Win64/UnrealEditor/Inc/CryptRaider/Mover.gen.cpp
@@ -37,11 +37,6 @@ void EmptyLinkFunctionForGeneratedCodeMover() {}
 		static const UECodeGen_Private::FMetaDataPairParam NewProp_MoveTime_MetaData[];
 #endif
 		static const UECodeGen_Private::FFloatPropertyParams NewProp_MoveTime;
-#if WITH_METADATA
-		static const UECodeGen_Private::FMetaDataPairParam NewProp_ShouldMove_MetaData[];
-#endif
-		static void NewProp_ShouldMove_SetBit(void* Obj);
-		static const UECodeGen_Private::FBoolPropertyParams NewProp_ShouldMove;
 		static const UECodeGen_Private::FPropertyParamsBase* const PropPointers[];
 		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
 		static const UECodeGen_Private::FClassParams ClassParams;
@@ -72,21 +67,9 @@ void EmptyLinkFunctionForGeneratedCodeMover() {}
 	};
 #endif
 	const UECodeGen_Private::FFloatPropertyParams Z_Construct_UClass_UMover_Statics::NewProp_MoveTime = { "MoveTime", nullptr, (EPropertyFlags)0x0040000000000001, UECodeGen_Private::EPropertyGenFlags::Float, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(UMover, MoveTime), METADATA_PARAMS(Z_Construct_UClass_UMover_Statics::NewProp_MoveTime_MetaData, UE_ARRAY_COUNT(Z_Construct_UClass_UMover_Statics::NewProp_MoveTime_MetaData)) };
-#if WITH_METADATA
-	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UMover_Statics::NewProp_ShouldMove_MetaData[] = {
-		{ "Category", "Mover" },
-		{ "ModuleRelativePath", "Mover.h" },
-	};
-#endif
-	void Z_Construct_UClass_UMover_Statics::NewProp_ShouldMove_SetBit(void* Obj)
-	{
-		((UMover*)Obj)->ShouldMove = 1;
-	}
-	const UECodeGen_Private::FBoolPropertyParams Z_Construct_UClass_UMover_Statics::NewProp_ShouldMove = { "ShouldMove", nullptr, (EPropertyFlags)0x0040000000000001, UECodeGen_Private::EPropertyGenFlags::Bool | UECodeGen_Private::EPropertyGenFlags::NativeBool, RF_Public|RF_Transient|RF_MarkAsNative, 1, sizeof(bool), sizeof(UMover), &Z_Construct_UClass_UMover_Statics::NewProp_ShouldMove_SetBit, METADATA_PARAMS(Z_Construct_UClass_UMover_Statics::NewProp_ShouldMove_MetaData, UE_ARRAY_COUNT(Z_Construct_UClass_UMover_Statics::NewProp_ShouldMove_MetaData)) };
 	const UECodeGen_Private::FPropertyParamsBase* const Z_Construct_UClass_UMover_Statics::PropPointers[] = {
 		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UMover_Statics::NewProp_MoveOffset,
 		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UMover_Statics::NewProp_MoveTime,
-		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UMover_Statics::NewProp_ShouldMove,
 	};
 	const FCppClassTypeInfoStatic Z_Construct_UClass_UMover_Statics::StaticCppClassTypeInfo = {
 		TCppClassTypeTraits<UMover>::IsAbstract,
@@ -124,9 +107,9 @@ void EmptyLinkFunctionForGeneratedCodeMover() {}
 		static const FClassRegisterCompiledInInfo ClassInfo[];
 	};
 	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_CryptRaider_Source_CryptRaider_Mover_h_Statics::ClassInfo[] = {
-		{ Z_Construct_UClass_UMover, UMover::StaticClass, TEXT("UMover"), &Z_Registration_Info_UClass_UMover, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(UMover), 3972742935U) },
+		{ Z_Construct_UClass_UMover, UMover::StaticClass, TEXT("UMover"), &Z_Registration_Info_UClass_UMover, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(UMover), 378527397U) },
 	};
-	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_CryptRaider_Source_CryptRaider_Mover_h_4045087614(TEXT("/Script/CryptRaider"),
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_CryptRaider_Source_CryptRaider_Mover_h_768074006(TEXT("/Script/CryptRaider"),
 		Z_CompiledInDeferFile_FID_CryptRaider_Source_CryptRaider_Mover_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_CryptRaider_Source_CryptRaider_Mover_h_Statics::ClassInfo),
 		nullptr, 0,
 		nullptr, 0);
--- CryptRaider/Source/CryptRaider/TriggerComponent.cpp
+++ CryptRaider/Source/CryptRaider/TriggerComponent.cpp
@@ -19,5 +19,41 @@ void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAct
 {
 
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-   
+
+    AActor *Actor = GetAcceptableActor();
+
+    if(Actor != nullptr){
+        Mover->SetShouldMove(true);
+    }
+    else
+    {
+        Mover->SetShouldMove(false);
+    }
+    
+
+
+}
+
+
+void UTriggerComponent::SetMover(UMover* NewMover)
+{
+    Mover = NewMover;
+}
+
+
+AActor* UTriggerComponent::GetAcceptableActor() const
+{
+    
+    TArray<AActor*> Actors;
+    GetOverlappingActors(Actors);
+
+    for (AActor* Actor : Actors) //Actor in Actors
+    {
+        if(Actor->ActorHasTag(AcceptableActorTag)){
+            UE_LOG(LogTemp, Display, TEXT("unlocking"));
+            return Actor;
+        }
+    }
+
+    return nullptr;
 }
